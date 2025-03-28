4fdd1683d9d6afdd436baaa5f4ad1462f26d5873
时博轩 <1316072138@bupt.edu.cn>
2025-03-28 20:05:42 +0800

    添加跨域配置

--- src/main/java/org/tinkerhub/offgo/config/WebConfig.java
+++ src/main/java/org/tinkerhub/offgo/config/WebConfig.java
@@ -5,11 +5,11 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.tinkerhub.offgo.handlerinterceptor.loginInterceptor;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry)
     {
-        registry.addInterceptor(new loginInterceptor()).addPathPatterns("/user/**");
+        registry.addInterceptor(new loginInterceptor()).addPathPatterns("/**");
     }
 
 }
