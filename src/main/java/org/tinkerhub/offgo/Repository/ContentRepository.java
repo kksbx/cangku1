50252b9fc973d3f46c84b1dc5388ace1c2bf0cf2
时博轩 <1316072138@bupt.edu.cn>
2025-04-15 16:40:15 +0800

    用户登录完成，尝试完成写入日记

--- src/main/java/org/tinkerhub/offgo/Repository/ContentRepository.java
+++ src/main/java/org/tinkerhub/offgo/Repository/ContentRepository.java
@@ -1,12 +1,15 @@
 package org.tinkerhub.offgo.Repository;
 
-import jakarta.persistence.Column;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.tinkerhub.offgo.entity.ContentEntity;
-import org.tinkerhub.offgo.entity.diary;
 
 @Configuration
-public interface ContentReposity extends JpaRepository<ContentEntity, Long> {
-    public ContentEntity findById(int id);
+public interface ContentRepository extends JpaRepository<ContentEntity, Long> {
+    ContentEntity findById(int id);
+    @Query("SELECT MAX(u.ID) FROM User u")
+    int findMaxID();
+
+
 }
--- src/main/java/org/tinkerhub/offgo/entity/ContentEntity.java
+++ src/main/java/org/tinkerhub/offgo/entity/ContentEntity.java
@@ -3,16 +3,15 @@ package org.tinkerhub.offgo.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="diary_content")
-public class diary_content {
+@Table(name="content")
+public class ContentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer diaryID;
-    private String diary_content;
-    public diary_content() {}
-    public diary_content(Integer diaryID, String diary_content) {
-        this.diaryID = diaryID;
-        this.diary_content = diary_content;
+    private Integer id;
+    private String content;
+    public ContentEntity() {}
+    public ContentEntity(Integer diaryID, String diary_content) {
+        this.id = diaryID;
+        this.content = diary_content;
     }
-
 }
